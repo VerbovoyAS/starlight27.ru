dfd250809c549728585b70ba0bc4df13d5037819
VerbovoyAS <66475919+VerbovoyAS@users.noreply.github.com>
2023-07-05 23:42:33 +0300

    * правка стилей по footer

--- footer.php
+++ footer.php
@@ -1,29 +1,15 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package hashtag
- */
-
-?>
 
 <div class="container px-2 px-sm-0">
     <footer class="bd-footer mt-5 bg-light shadow bg-body rounded-3 stars">
         <div class="container py-5">
             <div class="row">
-                <?php dynamic_sidebar( 'sidebar-footer-1' ); ?>
-                <?php dynamic_sidebar( 'sidebar-footer-2' ); ?>
-                <?php dynamic_sidebar( 'sidebar-footer-3' ); ?>
+                <div class="col-12 col-lg-3 offset-lg-1 mb-3"><?php dynamic_sidebar('sidebar-footer-1'); ?></div>
+                <div class="col-12 col-lg-4 mb-3"><?php dynamic_sidebar('sidebar-footer-2'); ?></div>
+                <div class="col-12 col-lg-4 mb-3 "><?php dynamic_sidebar('sidebar-footer-3'); ?></div>
             </div>
         </div>
     </footer>
 </div>
-
-
 
 <?php wp_footer();?>
 
